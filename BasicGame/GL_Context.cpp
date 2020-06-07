47ba668893c0a1dc877cdeb0a2bca421d9e0e771
tylerHanf <tball401@gmail.com>
2020-06-07 16:06:03 -0400

    Cleaned up extra code used for debug

--- BasicGame/GL_Context.cpp
+++ BasicGame/GL_Context.cpp
@@ -17,8 +17,6 @@
 
 GL_Context::GL_Context(LoadData *ld) { 
     init();
-    //testSetup();
-    //testTextLoad();
     loadVertsToGPU(ld->getVertices(), ld->getTexels(), ld->getVertIDs());
     loadTexToGPU(ld->getTextures(), ld->getTextIDs());
 }
@@ -45,24 +43,6 @@ void GL_Context::loadTexToGPU(std::vector<Texture> &textures, std::vector<GLuint
 
 void GL_Context::loadVertsToGPU(std::vector<std::vector<float>> &verts, std::vector<std::vector<float>> &texels, std::vector<int> &vertIDs) {
     int numVerts = verts.at(0).size();
-    /*
-    float vertCoord[(*verts)[0].size()];
-    float textCoord[(*texels)[0].size()];
-    memcpy(vertCoord, (*verts)[0].data(), (*verts)[0].size());
-    memcpy(textCoord, (*texels)[0].data(), (texels->at(0)).size());
-    for (int i=0; i<(*verts)[0].size()-3; i+=3) {
-        std::cout << (*verts)[0][i];
-        std::cout << (*verts)[0][i+1];
-        std::cout << (*verts)[0][i+2] << std::endl;
-    }
-    std::cout << "------------------------------------------------------" << std::endl;
-
-    for (int i=0; i<(*verts)[0].size()-3; i+=3) {
-        std::cout << vertCoord[i];
-        std::cout << vertCoord[i+1];
-        std::cout << vertCoord[i+2] << std::endl;
-    }
-    */
     numVBOs = verts.size() + texels.size();
     glGenVertexArrays(numVAOs, vao);
     glBindVertexArray(vao[0]);
@@ -75,19 +55,6 @@ void GL_Context::loadVertsToGPU(std::vector<std::vector<float>> &verts, std::vec
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
     glBufferData(GL_ARRAY_BUFFER, (texels.at(0)).size()*4, texels[0].data(), GL_STATIC_DRAW);
     vertIDs.push_back(0);
-    //Need to store vert buffer then texture buffer sequentually
-    //in VBO, but must not duplicate vbo indices
-    /*for (int i=0, j=0; i<verts->size(); i++, j+=2) {
-        //Load Verts
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[j]);
-        glBufferData(GL_ARRAY_BUFFER, (*verts)[i].size()*4, &(*verts)[i], GL_STATIC_DRAW);
-
-        //Load Texels
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[j+1]);
-        glBufferData(GL_ARRAY_BUFFER, (*texels)[i].size()*4, &(*texels)[i], GL_STATIC_DRAW);
-        vertIDs->push_back(j);
-    }
-    */
 }
 
 void GL_Context::init() {
@@ -123,8 +90,7 @@ void GL_Context::display(ObjectData objData) {
     aspect = (float)width / (float)height;
 
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
-    //vMat = glm::lookAt(camera->getPosition(), glm::vec3(8.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    //mMat = glm::translate(glm::mat4(1.0f), glm::vec3(8.0f, 0.0f, 0.0f));
+
     vMat = glm::lookAt(camera->getPosition(), player->getPosition(), glm::vec3(0.0f, 1.0f, 0.0f));
     mMat = glm::translate(glm::mat4(1.0f), player->getPosition());
 
@@ -143,13 +109,11 @@ void GL_Context::display(ObjectData objData) {
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, objData.getTextID(0));
-    //glBindTexture(GL_TEXTURE_2D, rockTex);
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
     glDrawArrays(GL_TRIANGLES, 0, player->getNumVertices());
-    //glDrawArrays(GL_TRIANGLES, 0, 12);
 }
 
 bool GL_Context::renderLoop(ObjectData objData) {
@@ -157,45 +121,5 @@ bool GL_Context::renderLoop(ObjectData objData) {
         display(objData);
         glfwSwapBuffers(window);
         glfwPollEvents();
-    }
-}
-
-void GL_Context::testSetup() {
-	float tetraPositions[36] = {
-                -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, -1.0f, -1.0f,
-                -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, 1.0f, 0.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-                1.0f, -1.0f, 1.0f, 0.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f
-        };
-
-	float tetraTexCoords[24] = {
-		0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f,
-		0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f
-	};
-
-	glGenVertexArrays(numVAOs, vao);
-	glBindVertexArray(vao[0]);
-	glGenBuffers(numVBOs, vbo);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(tetraPositions), tetraPositions, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(tetraTexCoords), tetraTexCoords, GL_STATIC_DRAW);
-}
-
-void GL_Context::testTextLoad() {
-    glEnable(GL_TEXTURE_2D);
-    img = SOIL_load_image("Assets/Textures/Space_Stars1.png", &imgWidth, &imgHeight, 0, SOIL_LOAD_RGB);
-    glGenTextures(1, &rockTex);
-    glBindTexture(GL_TEXTURE_2D, rockTex);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgWidth, imgHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    if (glewIsSupported("GL_EXT_texture_filter_anisotropic")) {
-        GLfloat anisoSetting = 0.0f;
-        glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &anisoSetting);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, anisoSetting);
     }
 }
